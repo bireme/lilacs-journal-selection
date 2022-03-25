f97885ac16b3c25d4430bb8213a4f3617105ada8
wilsonmoura <wilsonsmoura@gmail.com>
2022-03-24 23:58:03 -0300

    Update fixtures

---
+++ symphony/fixtures/data_list_publication_type.sql
@@ -0,0 +1,6 @@
+REPLACE INTO `list_publication_type` (`id`, `created`, `updated`, `name`, `slug`, `status`) VALUES (1,'2019-05-31 00:00:00','2019-05-31 00:00:00','Editorial','editorial',1);
+REPLACE INTO `list_publication_type` (`id`, `created`, `updated`, `name`, `slug`, `status`) VALUES (2,'2019-05-31 00:00:00','2019-05-31 00:00:00','Artigos originais','artigos-originais',1);
+REPLACE INTO `list_publication_type` (`id`, `created`, `updated`, `name`, `slug`, `status`) VALUES (3,'2019-05-31 00:00:00','2019-05-31 00:00:00','Artigos de revisão','artigos-de-revisao',1);
+REPLACE INTO `list_publication_type` (`id`, `created`, `updated`, `name`, `slug`, `status`) VALUES (4,'2019-05-31 00:00:00','2019-05-31 00:00:00','Relatos de casos','relatos-de-casos',1);
+REPLACE INTO `list_publication_type` (`id`, `created`, `updated`, `name`, `slug`, `status`) VALUES (5,'2019-05-31 00:00:00','2019-05-31 00:00:00','Cartas ao editor','cartas-ao-editor',1);
+REPLACE INTO `list_publication_type` (`id`, `created`, `updated`, `name`, `slug`, `status`) VALUES (6,'2019-05-31 00:00:00','2019-05-31 00:00:00','Outros','outros',1);
---
+++ symphony/fixtures/data_list_thematic_area.sql
@@ -0,0 +1,9 @@
+REPLACE INTO `list_thematic_area` (`id`, `created`, `updated`, `name`, `slug`, `status`) VALUES (1,'2019-05-31 00:00:00','2019-05-31 00:00:00','Ciências Exatas e da Terra','ciencias-exatas-e-da-terra',1);
+REPLACE INTO `list_thematic_area` (`id`, `created`, `updated`, `name`, `slug`, `status`) VALUES (2,'2019-05-31 00:00:00','2019-05-31 00:00:00','Ciências Biológicas','ciencias-biologicas',1);
+REPLACE INTO `list_thematic_area` (`id`, `created`, `updated`, `name`, `slug`, `status`) VALUES (3,'2019-05-31 00:00:00','2019-05-31 00:00:00','Engenharias','engenharias',1);
+REPLACE INTO `list_thematic_area` (`id`, `created`, `updated`, `name`, `slug`, `status`) VALUES (4,'2019-05-31 00:00:00','2019-05-31 00:00:00','Ciências da Saúde','ciencias-da-saude',1);
+REPLACE INTO `list_thematic_area` (`id`, `created`, `updated`, `name`, `slug`, `status`) VALUES (5,'2019-05-31 00:00:00','2019-05-31 00:00:00','Ciências Agrárias','ciencias-agrarias',1);
+REPLACE INTO `list_thematic_area` (`id`, `created`, `updated`, `name`, `slug`, `status`) VALUES (6,'2019-05-31 00:00:00','2019-05-31 00:00:00','Ciências Sociais Aplicadas','ciencias-sociais-aplicadas',1);
+REPLACE INTO `list_thematic_area` (`id`, `created`, `updated`, `name`, `slug`, `status`) VALUES (7,'2019-05-31 00:00:00','2019-05-31 00:00:00','Ciências Humanas','ciencias-humanas',1);
+REPLACE INTO `list_thematic_area` (`id`, `created`, `updated`, `name`, `slug`, `status`) VALUES (8,'2019-05-31 00:00:00','2019-05-31 00:00:00','Lingüística, Letras e Artes','lingueistica-letras-e-artes',1);
+REPLACE INTO `list_thematic_area` (`id`, `created`, `updated`, `name`, `slug`, `status`) VALUES (9,'2019-05-31 00:00:00','2019-05-31 00:00:00','Outros','outros',1);
